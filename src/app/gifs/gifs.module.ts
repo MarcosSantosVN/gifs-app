acf1a36a01d5c9b18a5097f4de2b00c610b41801
Marcos Santos <marcos.santos@viewnext.com>
2024-05-13 12:46:54 +0200

    Creado diseño de componentes de gifs

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { SearchBoxComponent } from './components/shearch-box/search-box.components';
+import { CardListComponent } from './components/card-list/card-list.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomePageComponent,
+    SearchBoxComponent,
+    CardListComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports:[
+    HomePageComponent
   ]
 })
 export class GifsModule { }
